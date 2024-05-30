7fa017837352e60046fb9523c8b1acb3c31d7c4f
HaiVQ <me@haivq.com>
2024-05-30 23:35:14 +0700

    create new path from category slug

--- downloader.py
+++ downloader.py
@@ -41,10 +41,46 @@ s3_client = boto3.client(
 )
 print('s3 client created')
 
+def get_taxonomy(post_id_list):
+    with db_conn.cursor() as cur:
+        cur.execute("""
+            SELECT DISTINCT t.term_id, tt.term_taxonomy_id, tt.parent, t.name, t.slug, tt.taxonomy
+            FROM wp_terms AS t
+            JOIN wp_term_taxonomy AS tt ON tt.term_id = t.term_id
+        """)
+        
+        term_taxonomy_result = cur.fetchall()
+        print('all terms fetched')
+        term_taxonomy_dict = {
+            term_id: taxonomy_id
+            for term_id, taxonomy_id, *_ in term_taxonomy_result
+        }
+        taxonomy_dict = {
+            taxonomy_id: (term_name, term_slug, taxonomy_name, term_taxonomy_dict.get(parent_id))
+            for _, taxonomy_id, parent_id, term_name, term_slug, taxonomy_name in term_taxonomy_result
+        }
+        print('taxonomy adjacent list built')
+        cur.execute("""
+            SELECT object_id, term_taxonomy_id
+            FROM wp_term_relationships
+            WHERE object_id IN %s
+        """, (post_id_list,))
+        term_relationship_list = cur.fetchall()
+        print('term relationship fetched')
+        post_taxonomy_dict = {}
+        for post_id, taxonomy_id in term_relationship_list:
+            post_taxonomy = post_taxonomy_dict.setdefault(post_id, {})
+            _, _, taxonomy_name, _ = taxonomy_dict[taxonomy_id]
+            post_taxonomy.setdefault(taxonomy_name, []).append(taxonomy_id)
+        
+        print('post taxonomy list has been built')
+
+        return taxonomy_dict, post_taxonomy_dict
+    
 def get_thumbnail_link():
     with db_conn.cursor() as cur:
         cur.execute("""
-            SELECT image_p.id, p.guid, image_p.guid
+            SELECT p.id, p.post_name, image_p.id, image_p.guid
             FROM wp_posts AS p
             JOIN wp_posts AS image_p
                 ON p.ID = image_p.post_parent
@@ -66,25 +102,34 @@ def put_image(image_id, image_url, s3_object_key):
         Key=s3_object_key,
         Body=img_content,
     )
-    if resp['ResponseMetadata']['HTTPStatusCode'] < 300:
+    if resp['ResponseMetadata']['HTTPStatusCode'] >= 300:
+        print(resp['ResponseMetadata'])
         raise Exception('failed to put data to S3')
+    print(f'image put to {s3_object_key}')
     return image_id, s3_object_key
 
 def main():
     post_thumb_list = get_thumbnail_link()
+    post_id_list = list(set(post_id for post_id, *_ in post_thumb_list))
+    taxonomy_dict, post_taxonomy_dict = get_taxonomy(post_id_list)
     params = []
+    post_image_counter_dict = {}
     with ThreadPoolExecutor() as executor:
         futures = []
-        for image_id, post_link, image_link in post_thumb_list:
-            parsed_post_url = urlparse(post_link)
-            image_obj_key = os.path.join('image', parsed_post_url.path.strip('/').strip())
+        for post_id, post_name, image_id, image_link in post_thumb_list:
+            post_category_id_list = post_taxonomy_dict.get(post_id, {}).get('product_cat', [])
+            term_slug_list = []
+            for category_id in post_category_id_list:
+                term_slug_list.append(taxonomy_dict[category_id][1])
+            image_number = post_image_counter_dict.setdefault(post_id, 1)
+            image_obj_key = os.path.join('3d-model', *term_slug_list, f'{post_name}-{str(image_number).rjust(3, "0")}.jpg')
+            post_image_counter_dict[post_id] = image_number + 1
             futures.append(executor.submit(put_image, image_id, image_link, image_obj_key))
 
         for future in as_completed(futures):
             image_id, image_obj_key = future.result()
             new_image_url = os.path.join(s3_cdn_url, image_obj_key)
             params.append((new_image_url, image_id))
-
     with db_conn.cursor() as cur:
         cur.executemany('UPDATE wp_posts SET guid=%s WHERE id=%s', params)
     
